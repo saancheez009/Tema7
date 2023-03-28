05f6683352cffdc4b10f6a5914357368b5e833c5
Britany <Britany@DESKTOP-S9E8VDF.home>
2023-03-28 23:07:35 +0200

    commit

--- Tema7_Constructores/src/ejercicio4/Artículo.java
+++ Tema7_Constructores/src/ejercicio4/Artículo.java
@@ -5,11 +5,11 @@ public class Artículo {
 	/**
 	 * Nombre del artículo
 	 */
-	 private String nombre;
+	 String nombre;
 	/**
 	 * precio del artículo
 	 */
-	 private int precio;
+	 int precio;
 	 /**
 	  * Variable global estática que es el IVA , ya que no cambia su valor
 	  */
@@ -17,7 +17,38 @@ public class Artículo {
 	 /**
 	  * cantidad de artículos que quedan en el almacén 
 	  */
-	 private int cuantosQuedan;
+	 int cuantosQuedan;
+	 
+	 public Articulo(String nombre, int precio, double IVA, int cuantosQuedan) {
+	        this.nombre = nombre;
+	        this.precio = precio;
+	        this.iva = 0.21;
+	        this.cuantosQuedan = cuantosQuedan;
+	    }
+	    public void imprimir(){
+	        System.out.println("Nombre: " + nombre + ", precio: " + precio + ",IVA: " + IVA + ", quedan en almacén: " + cuantosQuedan);
+	    }
+	    public int getPVP(){
+	        int num = (precio*IVA)/100;
+	        precio += num;
+	        return precio;
+	    }
+	    public int getPVPDescuento(int descuento){
+	        int desc = (precio*descuento)/100;
+	        precio += desc;
+	        return precio;
+	    }
+	    public boolean vender(int x){
+	        boolean vendido = false;
+	        if (cuantosQuedan-x>0){
+	            vendido = true;
+	            cuantosQuedan-=x;
+	        }
+	        return vendido;
+	    }
+	    public void almacenar(int x){
+	        cuantosQuedan+=x;
+	    }
 	 
 	 
 	 
--- Tema7_Constructores/src/ejercicio4/MainArtículo.java
+++ Tema7_Constructores/src/ejercicio4/MainArtículo.java
@@ -4,6 +4,46 @@ public class MainArtículo {
 
 	public static void main(String[] args) {
 		
+		
+		Articulo articulo = new Articulo("PC", 800, 10, 5);
+        int opc;
+        int cant = 0;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Que desea hacer");
+        System.out.println("1. Imprimir");
+        System.out.println("2. Obtener precio de venta al público con iva incluido");
+        System.out.println("3. Obtener precio de venta al público con descuento");
+        System.out.println("4. Vender");
+        System.out.println("5. Almacenar");
+        opc = sc.nextInt();
+        if (opc == 4 || opc == 5){
+            System.out.println("Introduzca la cantidad de articulos");
+            cant = sc.nextInt();
+        }
+        switch (opc){
+            case 1:
+                articulo.imprimir();
+                break;
+            case 2:
+                articulo.getPVP();
+                break;
+            case 3:
+                int desc;
+                System.out.println("Introduzca el porcentaje de descuento");
+                desc = sc.nextInt();
+                articulo.getPVPDescuento(desc);
+                break;
+            case 4:
+                articulo.vender(cant);
+                break;
+            case 5:
+                articulo.almacenar(cant);
+                break;
+            default:
+                System.out.println("Opción no válida");
+        }
+        sc.close();
+    
 
 	}
 
